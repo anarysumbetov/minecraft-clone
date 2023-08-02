9e4e2c4b91d85255aba032fa813e3c4452f0fb8b
Anarys Umbetov <brodiaga_sirim@mail.ru>
2023-08-02 21:35:17 +0600

    Cube component added

---
+++ src/components/Cubes.js
@@ -0,0 +1,14 @@
+import { useStore } from "../hooks/useStore.js";
+import { Cube } from "./Cube.js";
+
+export const Cubes = () => {
+    const [cubes] = useStore((state) => [
+        state.cubes
+    ])
+    console.log("Cubes: cubes:", cubes);
+    return cubes.map(({ key, pos, texture }) => {
+        return (
+            <Cube key={key} position={pos} texture={texture} />
+        )
+    });
+}
